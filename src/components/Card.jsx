049b9d589c61dab75284cb030bf899ab8fd861b8
Eve202 <eveahotin@gmail.com>
2024-12-19 12:09:20 +0100

    version v2 with verification

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import { Eye,  FilePenLine, Trash2 } from "lucide-react";
 const Card = ({ student , openModal }) => {
     return (
         <React.Fragment>
-            <div className={`w-40 h-48 border-t-2 ${ student['status'] === 'Nouveau' ? 'border-blue-500' : student['status'] === 'Transfert' ? 'border-cyan-500' : 'border-red-500' } bg-white shadow-md rounded-es-lg rounded-ee-lg flex flex-col justify-between`}>
+            <div className={`w-44 h-48 border-t-2 ${ student['status'] === 'Nouveau' ? 'border-blue-500' : student['status'] === 'Transfert' ? 'border-cyan-500' : 'border-red-500' } bg-white shadow-md rounded-es-lg rounded-ee-lg flex flex-col justify-between`}>
                 <div className='text-center flex flex-col gap-3 justify-center items-center h-full'>
                     <div className='bg-gray-100 p-5 rounded-full w-fit '><p className='text-4xl text-gray-800 '>{student['firstname'].slice(0, 1).toUpperCase()}{student['lastname'].slice(0, 1).toUpperCase()}</p></div>
                     <p className=''>{student['firstname'].toUpperCase()}</p>
